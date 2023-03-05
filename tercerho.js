1c31c0a1eeaa5353a0e3f98f898e7ebd7b826d7b
Yessiprogra <124018374+Yessiprogra@users.noreply.github.com>
2023-03-05 11:59:51 -0300

    Add files via upload

---
+++ tercerho.js
@@ -0,0 +1,4 @@
+document.getElementById('boton').onclick = function () {
+    console.log("capturamos el evento clik");
+document.getElementById("demo").innerHTML = "Brent Simmons 30años email:brentsimmons@gmail.com";
+}
